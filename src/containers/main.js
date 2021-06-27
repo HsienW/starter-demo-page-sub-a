bbd90e97eacb0d43012fe879817a452b8f4d6128
Wallace H Wu <wallace_h_wu@wistron.com>
2021-06-27 14:32:40 +0800

    update sub test 3

--- src/containers/main.js
+++ src/containers/main.js
@@ -6,6 +6,7 @@ export const Main = () => {
             <h2>Starter Demo Page Sub A is working!</h2>
             <h2>Sub A update test</h2>
             <h2>Sub A update test - 2</h2>
+            <h2>Sub A update test - 3</h2>
         </div>
     );
 };
